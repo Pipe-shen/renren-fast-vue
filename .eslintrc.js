0b550d6324b3a29b7f048b74015b709df5bd9022
Pipe-shen <yys320carter@163.com>
2020-02-28 16:21:51 +0800

    修改eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,21 @@
-// https://eslint.org/docs/user-guide/configuring
-
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    node: true
+  },
   parser: 'babel-eslint',
   parserOptions: {
     sourceType: 'module'
   },
-  env: {
-    browser: true,
-  },
-  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-  extends: 'standard',
-  // required to lint *.vue files
+  extends: [
+    'eslint:recommended'
+  ],
   plugins: [
     'html'
   ],
-  // add your custom rules here
   rules: {
-    // allow async-await
     'generator-star-spacing': 'off',
-    // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
 }
--- src/components/icon-svg/index.vue
+++ src/components/icon-svg/index.vue
@@ -1,6 +1,5 @@
 <template>
-  <svg
-    :class="getClassName"
+  <svg :class="getClassName"
     :width="width"
     :height="height"
     aria-hidden="true">
@@ -9,43 +8,43 @@
 </template>
 
 <script>
-  export default {
-    name: 'icon-svg',
-    props: {
-      name: {
-        type: String,
-        required: true
-      },
-      className: {
-        type: String
-      },
-      width: {
-        type: String
-      },
-      height: {
-        type: String
-      }
+export default {
+  name: "icon-svg",
+  props: {
+    name: {
+      type: String,
+      required: true
     },
-    computed: {
-      getName () {
-        return `#icon-${this.name}`
-      },
-      getClassName () {
-        return [
-          'icon-svg',
-          `icon-svg__${this.name}`,
-          this.className && /\S/.test(this.className) ? `${this.className}` : ''
-        ]
-      }
+    className: {
+      type: String
+    },
+    width: {
+      type: String
+    },
+    height: {
+      type: String
+    }
+  },
+  computed: {
+    getName() {
+      return `#icon-${this.name}`;
+    },
+    getClassName() {
+      return [
+        "icon-svg",
+        `icon-svg__${this.name}`,
+        this.className && /\S/.test(this.className) ? `${this.className}` : ""
+      ];
     }
   }
+};
 </script>
 
 <style>
-  .icon-svg {
-    width: 1em;
-    height: 1em;
-    fill: currentColor;
-    overflow: hidden;
-  }
+.icon-svg {
+  width: 1em;
+  height: 1em;
+  fill: currentColor;
+  overflow: hidden;
+}
 </style>
